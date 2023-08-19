28bc95ecbf4e1d81eb75f0babedce97aee2473e2
Ahmer Ali <aliahmer289@gmail.com>
2023-08-19 12:46:30 +0500

    UI changes

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from '@vercel/analytics/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <DndProvider backend={HTML5Backend}>
+  <React.StrictMode>
   <App />
-</DndProvider>,
+  <Analytics/>
+</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
