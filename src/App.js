c8614d870f22595dc22581d7606b8f7cd40e9b83
Ahmer Ali <aliahmer289@gmail.com>
2023-08-20 23:25:23 +0500

    mobile input issue resolved

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
 
-function GridCell({ label, isSelected, onClick }) {
+function GridCell({ label, isSelected, onClick, cellIndex }) {
   const cellClass = label ? "border border-gray-400" : "";
   const selectedClass = isSelected ? "bg-bg3" : "bg-bg2";
 
   return (
     <div
-      className={`md:h-16 h-10  md:w-20 sm:h-10 sm:w-2 flex items-center justify-center ${cellClass} ${selectedClass}`}
+      className={`md:h-16 h-10 md:w-20 sm:h-10 sm:w-2 flex items-center justify-center ${cellClass} ${selectedClass}`}
       onClick={onClick}
-    ></div>
+    >
+      {isSelected && (
+        <span className={`text-white${isSelected ? "text-sm" : ""}`}>
+          {`div${cellIndex + 1}`}
+        </span>
+      )}
+    </div>
   );
 }
+
+
 
 
 function App() {
@@ -20,49 +28,62 @@ function App() {
   const [selectedCells, setSelectedCells] = useState([]);
   const [isCodeVisible, setIsCodeVisible] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
+  const [selectedLabels, setSelectedLabels] = useState({});
+  const [gap, setGap] = useState(2); // Initial gap value
+
   const originalCols = 12;
 
   const generateGrid = () => {
     const grid = [];
-
+  
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
         const label = String.fromCharCode(65 + j);
         const isSelected = selectedCells.includes(`${i}-${j}`);
+        const cellIndex = selectedCells.indexOf(`${i}-${j}`);
         grid.push(
           <GridCell
             key={`${i}-${j}`}
             label={label}
             isSelected={isSelected}
             onClick={() => handleCellClick(i, j)}
+            cellIndex={cellIndex}
           />
         );
       }
-
+  
       for (let j = cols; j < originalCols; j++) {
         const isSelected = selectedCells.includes(`${i}-${j}`);
+        const cellIndex = selectedCells.indexOf(`${i}-${j}`);
         grid.push(
           <GridCell
             key={`${i}-${j}`}
             label=""
             isSelected={isSelected}
             onClick={() => handleCellClick(i, j)}
+            cellIndex={cellIndex}
           />
         );
       }
     }
-
+  
     return grid;
   };
+  
 
   const handleCellClick = (row, col) => {
     const cellKey = `${row}-${col}`;
-    if (selectedCells.includes(cellKey)) {
+    const selectedCellIndex = selectedCells.indexOf(cellKey);
+  
+    if (selectedCellIndex !== -1) {
       setSelectedCells(selectedCells.filter((key) => key !== cellKey));
     } else {
+      const nextLabel = `div${selectedCells.length + 1}`;
       setSelectedCells([...selectedCells, cellKey]);
+      setSelectedLabels({ ...selectedLabels, [cellKey]: nextLabel });
     }
   };
+  
 
   const generateTailwindClasses = () => {
     const classes = selectedCells.map((cellKey) => {
@@ -76,6 +97,22 @@ function App() {
 
     return classes.join("");
   };
+
+  // const generateTailwindClasses = () => {
+  //   const classes = selectedCells.map((cellKey) => {
+  //     const [row, col] = cellKey.split("-");
+  //     return `<div className='row-start-${parseInt(row) + 1} row-end-${
+  //       parseInt(row) + 2
+  //     } col-start-${parseInt(col) + 1} col-end-${
+  //       parseInt(col) + 2
+  //     } h-30 w-30 bg-blue-300'></div>\n`;
+  //   });
+  
+  //   const completeClasses = classes.join("");
+  //   const gapClasses = `grid-rows-${rows} grid-cols-${cols} gap-${gap}`;
+    
+  //   return `<div className="${gapClasses}">\n${completeClasses}</div>`;
+  // };
 
   const copyToClipboard = (text) => {
     const textField = document.createElement("textarea");
@@ -131,6 +168,18 @@ function App() {
       max="12"
       value={cols}
       onChange={(e) => setCols(Math.min(Math.max(e.target.value, 0), 12))}
+      className="border-gray-300 bg-bg2 text-white sm:w-20 border h-10 px-2 py-1 rounded"
+    />
+  </div>
+
+  <div className="mb-4 md:ml-4 sm:mb-0 sm:mr-4">
+    <span className="block sm:inline-block sm:mr-2">Gap</span>
+    <input
+      type="number"
+      id="gap"
+      min="0"
+      value={gap}
+      onChange={(e) => setGap(e.target.value)}
       className="border-gray-300 bg-bg2 text-white sm:w-20 border h-10 px-2 py-1 rounded"
     />
   </div>
@@ -197,12 +246,13 @@ function App() {
             </div>
             <div className="p-6 space-y-6">
               <pre className="text-base leading-relaxed text-gray-500 dark:text-gray-400 bg-bg2 p-2 rounded-md overflow-x-auto">
-                <code>
-                  {`<div className="grid grid-rows-${rows} grid-cols-${cols} gap-2">\n`}
-                  {generateTailwindClasses()}
-                  {`</div>`}
-                </code>
-              </pre>
+  <code>
+    {`<div className="grid grid-rows-${rows} grid-cols-${cols} gap-${gap}">\n`}
+    {generateTailwindClasses()}
+    {`</div>`}
+  </code>
+</pre>
+
               <button
                 className={`bg-bg3 text-white py-2 px-4 rounded-full ${
                   isCopied ? "bg-green-900" : ""
